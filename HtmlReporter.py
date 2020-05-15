8e40aea91caef860e8d3df1ec84619f6a5c2b7b7
fyering <17754070839@163.com>
2020-05-15 21:32:29 +0800

    update

--- HtmlReporter.py
+++ HtmlReporter.py
@@ -1,10 +1,11 @@
 import HTMLTestRunner
 import unittest
 from TestCase import TestClass
-
+import sys
 if __name__=='__main__':
     suite=unittest.makeSuite(TestClass)
-    filename='D:\\myreport.html'
+    filename='C:\\Program Files (x86)\\Jenkins\\workspace\\python_test\\test_report\\'+sys.argv[1]+".html"
+
     fp=open(filename,'wb')
-    runner=HTMLTestRunner.HTMLTestRunner(fp,title=u'my unit test',description=u'This is a report test')
+    runner = HTMLTestRunner.HTMLTestRunner(fp, title=u'my unit test', description=u'This is a report test')
     runner.run(suite)
